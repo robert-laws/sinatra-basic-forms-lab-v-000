c9f42f9ac3a522b65a0e06710d9040cfcbb894eb
Robert Laws <roblaws@me.com>
2017-10-25 11:57:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post "/create_puppy" do
+  post "/display_puppy" do
     @name = params[:name]
     @breed = params[:breed]
     @age = params[:age]
